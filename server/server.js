7c9be83d70139bb5d6750958a5e60f777b390550
Alistair Willis <alistairrwillis@gmail.com>
2017-01-30 12:59:45 +0900

    add get route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', function(req, res) {
+    Todo.find().then(function(todos) {
+        res.send({todos}); 
+    }, function(e) {
+        res.status(400).send(e); 
+    })
+}); //get
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = {app}; 
