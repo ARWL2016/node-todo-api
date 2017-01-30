9f180ee3d9f14013eeb0fecc1a330d022d17382b
Alistair Willis <alistairrwillis@gmail.com>
2017-01-30 19:00:58 +0900

    add tests for dynamic get request

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb'); 
 
 var {mongoose} = require('./db/mongoose'); 
 var {Todo} = require('./models/todo');
@@ -29,6 +30,29 @@ app.get('/todos', function(req, res) {
         res.status(400).send(e); 
     });
 }); //get
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id; 
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid ID'); 
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('todo not found');
+        } 
+
+        res.send({todo});     
+    }).catch((e) => {
+        res.status(400).send(); 
+    })
+}); 
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest'); 
+const {ObjectID} = require('mongodb'); 
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo'); 
 
 //dummy database data
 const todos = [{
+    _id: new ObjectID(), //generated here so we can access it in the test
     text: 'first test todo'
 }, {
+    _id: new ObjectID(),
     text: 'second test todo'
 }];
 
@@ -72,5 +75,35 @@ describe('GET /todos', function() {
                 expect(res.body.todos.length).toBe(2);
             })
             .end(done); 
+    });
+});
+
+describe('GET /todos/:id', function(){
+    it('should get a todo doc', function(done) {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect(function(res) {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done); 
+    });
+
+    it('should return 404 if todo not found', function(done) {
+        var id = new ObjectID(); 
+
+        request(app)
+            .get(`/todos/${id.toHexString}`)
+            .expect(404)
+            .end(done); 
+    });
+
+    it('should return 404 for non-object ids', function(done) {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+                
+
     });
 });
