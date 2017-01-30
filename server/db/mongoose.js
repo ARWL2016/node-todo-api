770915577e35c18edb18450e9aa9fb3a80043987
Alistair Willis <alistairrwillis@gmail.com>
2017-01-30 19:36:32 +0900

    setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // sets Mongoose to use built-in JS promises
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose}
