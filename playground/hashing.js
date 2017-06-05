395c03cbd010b74ae0d3fd74b0fe0277c2c36eae
Alistair Willis <alistairrwillis@gmail.com>
2017-06-05 13:13:35 +0900

    add

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,42 +1,43 @@
-const {SHA256} = require('crypto-js');
+const {SHA256, SHA512, md5} = require('crypto-js');
 const jwt = require('jsonwebtoken'); 
 
 var data = {
   id: 10
 };
 
-var token = jwt.sign(data, 'abc123'); 
-console.log(token); 
+// var token = jwt.sign(data, 'abc123'); 
+// console.log(token); 
 
-var decoded = jwt.verify(token, 'abc123'); 
-console.log('decoded', decoded); 
+// var decoded = jwt.verify(token, 'abc123'); 
+// console.log('decoded', decoded); 
 
-// var message = 'I am user number 10'; 
-// var hash = SHA256(message).toString(); 
+var message = 'I am user number 10'; 
+var hash = SHA256(message).toString(); 
+var hash2 = SHA512(message).toString(); 
 
-// console.log(`message`);
-// console.log(hash);
+console.log(`message`);
+console.log(hash2);
 
-// var data = {
-//   id: 4
-// }; 
-// // a token consists of a user id + a hashed version of that id 
-// // this prevents the user manipulating the id to access other data
-// var token = {
-//   data, 
-//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString() // converts object to hashed string
-// }
+var data = {
+  id: 4
+}; 
+// a token consists of a user id + a hashed version of that id 
+// this prevents the user manipulating the id to access other data
+var token = {
+  data, 
+  hash: SHA256(JSON.stringify(data) + 'somesecret').toString() // converts object to hashed string
+}
 
-// // Client attempts to manipulate data
-// token.data.id; 
-// token.hash = SHA256(JSON.stringify(token.data)).toString(); 
+// Client attempts to manipulate data
+token.data.id; 
+token.hash = SHA256(JSON.stringify(token.data)).toString(); 
 
-// // check the returning token
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString(); 
+// check the returning token
+var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString(); 
 
-// // validate 
-// if (resultHash === token.hash) {
-//   console.log('Data unchanged'); 
-// } else {
-//   console.log('data changed - do not trust');
-// }
+// validate 
+if (resultHash === token.hash) {
+  console.log('Data unchanged'); 
+} else {
+  console.log('data changed - do not trust');
+}
--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,7 @@ console.log('env ***', env);
 if (env === 'development') {
     process.env.PORT = 3000; 
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'; 
+    process.env.MLAB_URI = 'mongodb://arwl:87654321@ds163181.mlab.com:63181/mead-todo'
 } else if (env === 'test') {
     process.env.PORT = 3000; 
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
